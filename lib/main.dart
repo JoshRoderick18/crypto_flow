95f9e009a24af887b266fb843fb1c0bc9443c495
joshroderick18 <joshroderick18@gmail.com>
2026-01-28 14:16:17 -0300

    Se añaden las inicializaciones iniciales y la redirección a la HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'features/crypto/presentation/providers/crypto_providers.dart';
+import 'features/crypto/presentation/screens/home_screen.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  await Hive.initFlutter();
+  final box = await Hive.openBox('crypto_box');
+
+  runApp(ProviderScope(overrides: [cryptoBoxProvider.overrideWithValue(box)], child: const CoinVaultApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class CoinVaultApp extends StatelessWidget {
+  const CoinVaultApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Coin Vault',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
+      home: const HomeScreen(),
     );
   }
 }
