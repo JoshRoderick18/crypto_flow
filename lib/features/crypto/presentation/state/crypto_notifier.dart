9559e9e14a69f21cd629aad52fd4e5070fbc5762
joshroderick18 <joshroderick18@gmail.com>
2026-01-28 14:13:26 -0300

    Se añade el controlador

---
+++ lib/features/crypto/presentation/state/crypto_notifier.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:crypto_flow/features/crypto/domain/entities/crypto_entity.dart';
+import 'package:crypto_flow/features/crypto/presentation/providers/crypto_providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CryptoNotifier extends AutoDisposeAsyncNotifier<List<CryptoEntity>> {
+  List<CryptoEntity> _fullList = [];
+  Timer? _debounceTimer;
+
+  @override
+  Future<List<CryptoEntity>> build() async {
+    ref.onDispose(() => _debounceTimer?.cancel());
+
+    final repository = ref.read(cryptoRepositoryProvider);
+
+    _fullList = await repository.getCryptos();
+
+    return _fullList;
+  }
+
+  void search(String query) {
+    if (_debounceTimer?.isActive ?? false) _debounceTimer!.cancel();
+
+    _debounceTimer = Timer(const Duration(milliseconds: 500), () {
+      if (query.isEmpty) {
+        state = AsyncValue.data(_fullList);
+        return;
+      }
+
+      final filtered = _fullList.where((coin) {
+        return coin.name.toLowerCase().contains(query.toLowerCase()) || coin.symbol.toLowerCase().contains(query.toLowerCase());
+      }).toList();
+
+      state = AsyncValue.data(filtered);
+    });
+  }
+
+  Future<void> refresh() async {
+    ref.invalidateSelf();
+    await future;
+  }
+}
