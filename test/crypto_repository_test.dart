7978179cc65527617ceba164094b13b969047057
joshroderick18 <joshroderick18@gmail.com>
2026-01-28 14:26:50 -0300

    Se añade el test para el repositorio de crypto

---
+++ test/crypto_repository_test.dart
@@ -0,0 +1,32 @@
+import 'package:crypto_flow/features/crypto/data/datasources/crypto_local_data_source.dart';
+import 'package:crypto_flow/features/crypto/data/datasources/crypto_remote_data_source.dart';
+import 'package:crypto_flow/features/crypto/data/models/crypto_model.dart';
+import 'package:crypto_flow/features/crypto/data/repositories/crypto_repository_impl.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockRemote extends Mock implements CryptoRemoteDataSource {}
+
+class MockLocal extends Mock implements CryptoLocalDataSource {}
+
+void main() {
+  late CryptoRepositoryImpl repo;
+  late MockRemote mockRemote;
+  late MockLocal mockLocal;
+
+  setUp(() {
+    mockRemote = MockRemote();
+    mockLocal = MockLocal();
+    repo = CryptoRepositoryImpl(mockRemote, mockLocal);
+  });
+
+  test('Debe usar datos locales si falla el remoto', () async {
+    when(() => mockRemote.getAll()).thenThrow(Exception());
+    when(() => mockLocal.getLastCryptos()).thenAnswer((_) async => [const CryptoModel(id: '1', symbol: 'x', name: 'x', imageUrl: 'x', currentPrice: 1)]);
+
+    final result = await repo.getCryptos();
+
+    expect(result.length, 1);
+    verify(() => mockLocal.getLastCryptos()).called(1);
+  });
+}
